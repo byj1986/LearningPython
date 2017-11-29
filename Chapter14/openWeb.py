ca765f994238881107366fcf403b436dea32785c
byj1986 <byj1986@163.com>
2017-11-30 01:13:16 +0800

    Add demos of Chapter 14

---
+++ Chapter14/openWeb.py
@@ -0,0 +1,23 @@
+import re
+import urllib
+
+url = 'http://www.python.org'
+webpage = urllib.urlopen(url)
+text = webpage.read()
+# print(webpage)
+# print(text)
+
+m = re.search('<a href="([^"]+)" .*?>about</a>', text, re.IGNORECASE)
+print m.group(1)
+
+urllib.urlretrieve(url, "python.html")
+
+urllib.urlcleanup()
+
+# http://www.w3school.com.cn/tags/html_ref_urlencode.html
+# refer the html for more information about html url coding/encoding
+word = urllib.quote(' ')
+print word
+
+word = urllib.unquote('%3d')
+print word
---
+++ Chapter14/server.py
@@ -0,0 +1,15 @@
+import socket
+
+s = socket.socket()
+
+host = socket.gethostname()
+print host
+port = 1234
+s.bind((host, port))
+s.listen(5)
+
+while True:
+    c, address = s.accept()
+    print 'Got connection form', address
+    c.send('Thank you for connecting')
+    c.close()
