7eebab3f1fc020efb166211a7298955681a17a21
byj1986 <byj1986@163.com>
2019-01-29 21:47:34 +0800

    make salary calculator reuseable

--- SalaryCalculator/salaryCalculator.py
+++ SalaryCalculator/salaryCalculator.py
@@ -6,27 +6,25 @@ from SubItems import Pension
 from SubItems import Medical
 from SubItems import Tax
 
-# salaries = [5000, 10000, 15000, 20000, 25000, 30000, 50000, 80000, 100000, 1000000]
-# for salary in salaries:
-#     pension = Pension.pension(salary)
-#     medical = Medical.medical(salary)
-#     unemployment = Unemployment.unemployment(salary)
-#     house = House.house(salary)
-#     payTax = round(salary - pension - medical - unemployment - house, 2)
-#     tax = Tax.tax(payTax)
-#     print("Pre-tax: " + str(salary) + " Net salary: " + str(round(payTax - tax, 2)))
+
+def calcSalary(salary, deduction=0):
+    pension = Pension.pension(salary)
+    medical = Medical.medical(salary)
+    unemployment = Unemployment.unemployment(salary)
+    house = House.house(salary)
+    payTax = round(salary - pension - medical - unemployment - house, 2)
+    withDeductionTax = Tax.tax(payTax, deduction)
+    withoutDeductionTax = Tax.tax(payTax)
+    deductionSave = round(withoutDeductionTax - withDeductionTax, 2)
+    netSalary = round(payTax - withDeductionTax, 2)
+    return [deductionSave, netSalary]
 
 
-salary = 30000
-deduction = 3000
+if __name__ == '__main__':
+    paySalaries = [5000, 8000, 10000, 15000, 20000, 25000, 30000, 50000, 80000, 100000]
 
-pension = Pension.pension(salary)
-medical = Medical.medical(salary)
-unemployment = Unemployment.unemployment(salary)
-house = House.house(salary)
-payTax = round(salary - pension - medical - unemployment - house, 2)
-withDeductionTax = Tax.tax(payTax, deduction)
-withoutDeductionTax = Tax.tax(payTax)
-deductionSave = withoutDeductionTax - withDeductionTax
-print("Deduction save you " + str(round(deductionSave, 2))+" per month.")
-print("Pre-tax: " + str(salary) + " Net salary: " + str(round(payTax - withDeductionTax, 2)))
+    for salary in paySalaries:
+        result = calcSalary(salary, 2000)
+        print("Deduction save you " + str(result[0]) + " per month.")
+        print("Pre-tax: " + str(salary) + " Net salary: " + str(result[1]))
+        print()
