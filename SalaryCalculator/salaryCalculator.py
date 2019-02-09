f27c3b7aaf6fdec812e6ef0429af22df30fbb86d
byj1986 <byj1986@163.com>
2019-02-09 20:15:35 +0800

    update import statement

--- SalaryCalculator/salaryCalculator.py
+++ SalaryCalculator/salaryCalculator.py
@@ -1,27 +1,28 @@
 # coding=utf-8
 
-from SubItems import House
-from SubItems import Unemployment
-from SubItems import Pension
-from SubItems import Medical
-from SubItems import Tax
+
+from House import *
+from Unemployment import *
+from Pension import *
+from Medical import *
+from Tax import *
 
 
 def calcSalary(salary, deduction=0):
-    pension = Pension.pension(salary)
-    medical = Medical.medical(salary)
-    unemployment = Unemployment.unemployment(salary)
-    house = House.house(salary)
-    payTax = round(salary - pension - medical - unemployment - house, 2)
-    withDeductionTax = Tax.tax(payTax, deduction)
-    withoutDeductionTax = Tax.tax(payTax)
+    pensionRes = pension(salary)
+    unemploymentRes = unemployment(salary)
+    medicalRes = medical(salary)
+    houseRes = house(salary)
+    payTax = round(salary - pensionRes - unemploymentRes - medicalRes - houseRes, 2)
+    withDeductionTax = tax(payTax, deduction)
+    withoutDeductionTax = tax(payTax)
     deductionSave = round(withoutDeductionTax - withDeductionTax, 2)
     netSalary = round(payTax - withDeductionTax, 2)
     return [deductionSave, netSalary]
 
 
 if __name__ == '__main__':
-    paySalaries = [5000, 8000, 10000, 15000, 20000, 22680, 25000, 30000, 50000, 80000, 100000]
+    paySalaries = [5000, 8000, 10000, 15000, 20000, 25000, 30000, 50000, 80000, 100000]
 
     for salary in paySalaries:
         result = calcSalary(salary, 2000)
