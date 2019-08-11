cd47856101f0bd830cdaf1c2a1d35df5293ff063
byj1986 <byj1986@163.com>
2019-08-11 23:36:28 +0800

    simplify code

--- JsonPropertyFilter/JsonFilter.py
+++ JsonPropertyFilter/JsonFilter.py
@@ -2,31 +2,12 @@ import json
 import sys
 
 
-def resolve_Json(filepath):
+def resolve_json(filepath):
     file = open(filepath, "rb")
     return json.load(file)
 
 
-def filter_Person(source, condition):
-    filtered_persons = []
-    for obj in source:
-        if eval(condition):
-            filtered_persons.append(obj)
-    return filtered_persons
-    # return filter(person_filter(,condition), source)
-    # res = eval(condition)
-    # print(res)
-    # return res
-    # for x in conditions:
-    #     print(x)
-    # return [element for element in source if element['Married'] == married]
-    # return [element for element in source if eval(condition)]
-    # "obj['Age'] >= 30 and obj['Married'] == True"
-
-
 if __name__ == '__main__':
-    # filepath = r"SampleData.json"
-    print(sys.argv)
-    result = filter_Person(resolve_Json(sys.argv[1]), sys.argv[2])
+    result = [obj for obj in resolve_json(sys.argv[1]) if eval(sys.argv[2])]
     for x in result:
         print(x)
