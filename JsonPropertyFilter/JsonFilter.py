c77fdbe98978078d4f96dfd201aaf849b32aaead
byj1986 <byj1986@163.com>
2019-08-11 23:53:36 +0800

    add conditions for child nodes.

--- JsonPropertyFilter/JsonFilter.py
+++ JsonPropertyFilter/JsonFilter.py
@@ -3,7 +3,13 @@ import sys
 
 if __name__ == '__main__':
     # "C:\\Codes\\Python\\LearningPython\\JsonPropertyFilter\\SampleData.json"
-    # "obj['Gender']== 'Male' or (obj['Age']>=30 and obj['Married'] == True)"
+    condition = "len(obj['Children']) > 0"
+
+    # persons = json.load(open(sys.argv[1], 'rb'))
+    # for x in persons:
+    #     print(len(x["Children"]))
+
+    # result = [obj for obj in json.load(open(sys.argv[1], 'rb')) if eval(condition)]
     result = [obj for obj in json.load(open(sys.argv[1], 'rb')) if eval(sys.argv[2])]
     for x in result:
         print(x)
