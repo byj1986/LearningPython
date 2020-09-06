789338116f9a4abdb14c58f97ded91b61433e784
byj1986 <byj1986@163.com>
2020-09-06 11:47:51 +0800

    refact codes

--- StockMarket/excelOp.py
+++ StockMarket/excelOp.py
@@ -28,9 +28,65 @@ def getExcelSheet(workBook, sheetName):
         sheet = workBook[sheetName]
     else:
         sheet = workBook.create_sheet(sheetName)
-        # TODO Copy from first column in first workSheet
-        
+        applyDefaultTemplate(sheet)
     return sheet
+
+
+def applyDefaultTemplate(sheet):
+    """
+    Apply default template to the new sheet
+    """
+    sheet.cell(2, 1).value = '当日成本'
+    sheet.cell(5, 1).value = '20日成本'
+    sheet.cell(8, 1).value = '60日成本'
+    sheet.cell(11, 1).value = '机构参与度'
+
+
+def getRoundedNumber(rawData: any, precision: int = 2) -> float:
+    return round(float(rawData), precision)
+
+
+def updateExcel(stocks: any):
+    for stock in stocks:
+        # print(stock)
+        cywData = getCYWDailyData(stock['id'])
+        # print(cywData)
+        tradeDate = getTradeDate(cywData)
+        # print(tradeDate.ctime())
+        # print(getTradeDate(cywData))
+        stockName = cywData['Name']
+
+        sheet = getExcelSheet(wb, stockName)
+        columns = sheet.max_column
+        lastDate = None
+        if sheet.cell(1, columns).value:
+            lastDate = sheet.cell(1, columns).value.date()
+
+        # get last cell of row 1
+        # if last cell equals cywData's date, update the cell
+        # else create a new column
+        columnNumber = columns+1
+        if lastDate and lastDate == tradeDate:
+            columnNumber = columns
+        # print('Column number is ' + str(columnNumber))
+
+        sheet.cell(1, columnNumber).value = tradeDate
+        sheet.cell(1, columnNumber).data_type = 'd'
+        sheet.cell(1, columnNumber)._style = dateStyle
+
+        sheet.cell(2, columnNumber).value = getRoundedNumber(
+            cywData['ZLCB'])
+        sheet.cell(5, columnNumber).value = getRoundedNumber(
+            cywData['ZLCB20R'])
+        sheet.cell(8, columnNumber).value = getRoundedNumber(
+            cywData['ZLCB60R'])
+        sheet.cell(11, columnNumber).value = getRoundedNumber(
+            cywData['JGCYD'])
+        # break
+        # Step 2: put data and update formula in excel
+
+    # Step 3: save excel
+    wb.save(xlsFile)
 
 
 if __name__ == "__main__":
@@ -40,50 +96,4 @@ if __name__ == "__main__":
     # Step 1: Get Data for yesterday for one stock
     with open(stockJson, 'r', encoding='utf-8') as stockFile:
         stocks = json.load(stockFile)
-        for stock in stocks:
-            # print(stock)
-            cywData = getCYWDailyData(stock['id'])
-            # print(cywData)
-            tradeDate = getTradeDate(cywData)
-            # print(tradeDate.ctime())
-            # print(getTradeDate(cywData))
-            stockName = cywData['Name']
-
-            sheet = getExcelSheet(wb, stockName)
-            columns = sheet.max_column
-            rows = sheet.max_row
-            rowCount = sheet.rows
-            # lastDate = sheet.cell(1, columns).value.date()
-            lastDate = None
-            if sheet.cell(1, columns).value:
-                lastDate = sheet.cell(1, columns).value.date()
-
-            # get last cell of row 1
-            # if last cell equals cywData's date, update the cell
-            # else create a new column
-            columnNumber = columns+1
-            if lastDate and lastDate == tradeDate:
-                columnNumber = columns
-            # print('Column number is ' + str(columnNumber))
-
-            sheet.cell(1, columnNumber).value = tradeDate
-            sheet.cell(1, columnNumber).data_type = 'd'
-            sheet.cell(1, columnNumber)._style = dateStyle
-            zlcb = float(cywData['ZLCB'])
-            zlcb20 = float(cywData['ZLCB20R'])
-            zlcb60 = float(cywData['ZLCB60R'])
-            roundedZlcb = round(zlcb, 2)
-            roundedZlcb20 = round(zlcb20, 2)
-            roundedZlcb60 = round(zlcb60, 2)
-            # print(zlcb, roundedZlcb)
-
-            sheet.cell(2, columnNumber).value = roundedZlcb
-            sheet.cell(5, columnNumber).value = roundedZlcb20
-            sheet.cell(8, columnNumber).value = roundedZlcb60
-            # break
-            # print(getCWY(cywData, 'ZLCB20R'))
-            # Step 2: put data and update formula in excel
-            # print(cywData)
-
-    # Step 3: save excel
-    wb.save(xlsFile)
+        updateExcel(stocks)
